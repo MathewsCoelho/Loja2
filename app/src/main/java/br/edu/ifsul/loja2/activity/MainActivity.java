061a607406bdfdef15c4b704afb07f28a5bc0daf
Your Name <matheusbarcelos.c@gmail.com>
2019-08-16 11:12:24 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifsul/loja2/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package br.edu.ifsul.loja2.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.edu.ifsul.loja2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
