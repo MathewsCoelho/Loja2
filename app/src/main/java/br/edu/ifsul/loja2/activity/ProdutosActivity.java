4aa634351410c3d07b07da89bd852e7508e3a7f5
lucas <lucas_buttow@outlook.com>
2019-10-08 10:45:59 -0300

    Add barcode lib

--- app/src/main/java/br/edu/ifsul/loja2/activity/ProdutosActivity.java
+++ app/src/main/java/br/edu/ifsul/loja2/activity/ProdutosActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -27,6 +31,8 @@ public class ProdutosActivity extends AppCompatActivity {
 
     private static final String TAG = "produtosActivity";
     private ListView lvProdutos;
+    private List<Produto> produtosTemp = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,12 +41,18 @@ public class ProdutosActivity extends AppCompatActivity {
         //mapeia o componente da view
         lvProdutos = findViewById(R.id.lv_produtos);
 
-        lvProdutos.setOnItemClickListener(new AdapterView.OnItemClickListener () {
+        lvProdutos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(ProdutosActivity.this, ProdutoDetalheActivity.class);
-                intent.putExtra("position", i);
-                startActivity(intent);
+                if (produtosTemp.isEmpty()) {
+                    Intent intent = new Intent(ProdutosActivity.this, ProdutoDetalheActivity.class);
+                    intent.putExtra("position", i);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(ProdutosActivity.this, ProdutoDetalheActivity.class);
+                    intent.putExtra("position", produtosTemp.get(i).getIndex());
+                    startActivity(intent);
+                }
             }
         });
 
@@ -52,7 +64,7 @@ public class ProdutosActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Log.d(TAG, "dataSnapshot=" + dataSnapshot);
                 AppSetup.listProdutos = new ArrayList<>();
-                for(DataSnapshot ds : dataSnapshot.getChildren()){
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     Log.d(TAG, "dataSnapshot=" + ds);
                     Produto produto = ds.getValue(Produto.class);
                     produto.setKey(ds.getKey());
@@ -69,6 +81,48 @@ public class ProdutosActivity extends AppCompatActivity {
 
             }
         });
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_produtos, menu);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.menuitem_pesquisar).getActionView();
+        searchView.setQueryHint(getString(R.string.text_nome));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                produtosTemp.clear();
+                for(Produto p : AppSetup.listProdutos){
+                    if(p.getNome().contains(newText)){
+                        produtosTemp.add(p);
+                    }
+                }
+
+                //faz o bindView
+                lvProdutos.setAdapter(new ProdutosAdapter(ProdutosActivity.this, produtosTemp));
+
+                return true;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuitem_barcode:
+                Toast.makeText(this, "código de barras", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return true;
+    }
+
 }
